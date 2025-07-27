4649c02e5b1cd3fc05c8697ba30053128d97d410
ctornatta <chris@tornatta.net>
2025-07-27 17:34:07 -0400

    Added barbar keymappings

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -131,6 +131,20 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- this is for the barbar plugin. The "A" is the ALT key so ALT-, and ALT-. to move the buffers
 vim.keymap.set('n', '<A-,>', '<Cmd>BufferPrevious<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<A-.>', '<Cmd>BufferNext<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<A-<>', '<Cmd>BufferPrevious<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<A->>', '<Cmd>BufferNext<CR>', { noremap = true, silent = true })
 
+-- Enable later for now commenting out
+-- vim.keymap.set('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', { noremap = true, silent = true })
+-- map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+-- map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+-- map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+-- map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+-- map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+-- map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+-- map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+-- map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+-- map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
