aac95fbca85ee13449dd7b9bc4806182f0d212cd
ctornatta <chris@tornatta.net>
2025-07-31 06:22:30 -0400

    Added the render markdown plugin

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,16 @@
+-- Render Markdown
+-- https://github.com/MeanderingProgrammer/render-markdown.nvim
+
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    keys = {
+        {
+            "<leader>mr",
+            "<cmd>RenderMarkdown toggle<cr>",
+            desc = "[M]arkdown [R]ender toggle",
+            ft = "markdown",
+        },
+    },
+}
