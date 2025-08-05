99798cdc9b8a90b24af0e614677e2ae246808eae
Chris Tornatta <chris@tornatta.net>
2025-08-05 11:46:08 -0400

    Updated lualine to show the relative path of the file in the status bar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -24,7 +24,12 @@ return {
       sections = {
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
+        lualine_c = {
+          {
+            'filename',
+            path = 1  -- 0 = just filename, 1 = relative path, 2 = absolute path
+          }
+        },
         lualine_x = {'encoding', 'fileformat', 'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
@@ -32,7 +37,12 @@ return {
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = {'filename'},
+        lualine_c = {
+          {
+            'filename',
+            path = 1
+          }
+        },
         lualine_x = {'location'},
         lualine_y = {},
         lualine_z = {}
